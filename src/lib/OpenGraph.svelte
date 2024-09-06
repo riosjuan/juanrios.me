0336bb7089b78af985881183dc00617c84749743
Juan <3447894+riosjuan@users.noreply.github.com>
2024-09-06 18:27:51 +0200

    Updated OpenGraph component

--- src/lib/OpenGraph.svelte
+++ src/lib/OpenGraph.svelte
@@ -1,8 +1,10 @@
 <script>
-	export let title;
-	export let description;
-	export let url;
-	export let ogImage;
+	export let title = '';
+	export let description = '';
+	export let url = '';
+	export let ogImage = '';
+
+	$: fullImageUrl = `${url}/${ogImage}`;
 </script>
 
 <svelte:head>
@@ -11,16 +13,17 @@
 	<!-- Google / Search Engine Tags -->
 	<meta itemprop="name" content={title} />
 	<meta itemprop="description" content={description} />
-	<meta itemprop="image" content={`${url}/${ogImage}`} />
+	<meta itemprop="image" content={fullImageUrl} />
 	<!-- Facebook Meta Tags -->
 	<meta property="og:url" content={url} />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={`${url}/${ogImage}`} />
+	<meta property="og:image" content={fullImageUrl} />
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={`${url}/${ogImage}`} />
+	<meta name="twitter:image" content={fullImageUrl} />
+	<meta name="twitter:site" content="@juanriosnl" />
 </svelte:head>
